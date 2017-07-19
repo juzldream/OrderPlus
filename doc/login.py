a9c5c6d276295938821c3cd2fcaf3f8487ae77aa
rzhou <1576768715@qq.com>
2017-07-19 18:13:53 +0800

    增加2017/07/19工作内容

--- doc/login.py
+++ doc/login.py
@@ -18,11 +18,11 @@ url = 'http://192.168.149.131:8080/appform/ssologin'
 
 
 
-token = 'EF7F0BD62D851C74A760E596731B77F3445B5D69C7BAA46EEAE948791D7AFD8066C49C89199580BD460A381305F1F6AFE8D3E669AA2ADA5B6FF8798B95BF243F318E908D8872A92949872BA461CF9D25D4B94AB37DB517B6BC173E7F19DEE059'
+token = '72B7656A987E7C74DA2C9C2A947FC3B4445B5D69C7BAA46EEAE948791D7AFD8066C49C89199580BD460A381305F1F6AFE8D3E669AA2ADA5B6FF8798B95BF243FDB99252708C76BFABCD453BA3C97E420D572D6BE24528E168C40E6F872ACD388'
 
 # 同步启用信息 [type ? 0 : 新增或修改；1：删除]
 url = 'http://192.168.149.131:8080/appform/ws/user/syncDep'
-d = {"sysOrgInfoId":"10003","name":"营销部","type":"0","token":token}
+d = {"sysOrgInfoId":"10003","name":"营销部99","type":"0","token":token}
 # r = requests.post(url,data=d)
 # print r.text
 # print r.json()
@@ -30,13 +30,13 @@ d = {"sysOrgInfoId":"10003","name":"营销部","type":"0","token":token}
 
 # 同步人员信息
 url = 'http://192.168.149.131:8080/appform/ws/user/syncUser' 
-d = {"id":"002","name":"jhinno1","identityCard":"612526199302101000","role":"Admin","unitName":"otherDep","token":token}
-# r = requests.post(url,data=d)
-# print r.text
+d = {"id":"010","name":"jhinno3","identityCard":"008","role":"Admin","unitName":"otherDep","token":token}
+r = requests.post(url,data=d)
+print r.text
 
 
 # 修改用户状态 [0:freezing;1:normal]
 url = 'http://192.168.149.131:8080/appform/ws/user/updateUserStatus'
-d = {"status":0,"userId":"jhinno1","token":token} 
-s = requests.post(url,data=d)
-print s.text
+d = {"status":1,"userId":"006","token":token} 
+# s = requests.post(url,data=d)
+# print s.text
